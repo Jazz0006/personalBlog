7a4c8e18e472f75bb886ecf1a7cdb2ce9d8c4c46
JazzZeng <jazzzeng@LAPTOP-ASUS.localdomain>
2021-12-13 01:15:09 +1100

    Simple schema validator

--- program/schemas/blog_schema.py
+++ program/schemas/blog_schema.py
@@ -2,10 +2,11 @@ from marshmallow_sqlalchemy.schema import auto_field
 from main import ma
 from models.blogs import Blog
 from marshmallow_sqlalchemy import auto_field
+from marshmallow.validate import Length
 
 class BlogSchema(ma.SQLAlchemySchema):
     blog_id = auto_field(dump_only=True)
-    blog_title = auto_field(Required = True)
+    blog_title = auto_field(Required = True, validate=Length(min=1))
     blog_content = auto_field()
     blog_created = auto_field()
 
